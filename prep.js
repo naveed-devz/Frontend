ff84624bec7a2bfd99644c8a723fcab7cec562a2
NAVEED476 <snaveedn81@gmail.com>
2024-10-14 10:35:53 +0530

    frontend interview prep

---
+++ prep.js
@@ -0,0 +1,82 @@
+// call apply bind
+
+
+// let person = {
+//     fname:"naveed",
+//     lname:"shaik",
+//     fullname:function(){
+//         return   this.fname + this.lname ;
+//     }
+// }
+// let person2 = {
+//     fname:"jhon",
+//     lname:'doe'
+// }
+
+// // console.log(person.fullname.call(person2))
+// // console.log(person.fullname.apply(person2,["hi" , "hello"]))
+// const bindMethod = person.fullname.bind(person2);
+
+// console.log(bindMethod());
+
+
+
+// shallow copy ---> original array toplevel prepoerties wont be affected but nested will be updated when we update the copied obj
+
+// let obj={
+//     name:"naveed",
+//     details:{
+//         age:25
+//     }
+// }
+
+// let obj2 = Object.assign({},obj);
+
+// obj2.name="shaik";
+// obj2.details.age=30
+
+// console.log(obj,"original obj");
+// console.log(obj2,"copied obj");
+
+//deepcopy 
+
+
+// let obj={
+//     name:"naveed",
+//     details:{
+//         age:25
+//     }
+// }
+
+// let obj2 = JSON.parse(JSON.stringify(obj));
+
+// obj2.name ="shaik";
+// obj2.details.age=30;
+
+// console.log("original",obj);
+// console.log("copied",obj2);
+
+
+
+//clousure
+
+
+function counter (){
+    let count = 0;
+
+    return{
+        increment:function(){count++
+            console.log(count);
+        },
+        decrement:function(){
+            count--
+            console.log(count)
+        }
+    }
+}
+
+const myCounter = counter();
+
+myCounter.increment();
+myCounter.increment();
+myCounter.decrement();
